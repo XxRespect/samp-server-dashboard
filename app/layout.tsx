93784e0fcc4d68b0c9319d9441413d13034294fc
Jan Karlos <jankarlos512@gmail.com>
2026-04-15 22:10:37 +0100

    tRCP setup and set

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from '@/components/theme-provider';
 import { Providers } from '@/providers/authProvider';
-
-import AppFooter from '@/components/AppFooter'
+import { TRPCReactProvider } from '@/trpc/client';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,17 +32,19 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <Providers>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-   
-        </ThemeProvider>
-        </Providers>
+        <TRPCReactProvider>
+          <Providers>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="dark"
+              enableSystem
+              disableTransitionOnChange
+            >
+              {children}
+
+            </ThemeProvider>
+          </Providers>
+        </TRPCReactProvider>
       </body>
     </html>
   );
